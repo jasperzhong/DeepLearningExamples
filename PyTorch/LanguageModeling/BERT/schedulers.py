0835f6bbb8f7906a273bf6e0704bf1467c9003fe
vycezhong <izhongyuchen@gmail.com>
2020-12-13 00:10:42 +0800

    debug

--- PyTorch/LanguageModeling/BERT/schedulers.py
+++ PyTorch/LanguageModeling/BERT/schedulers.py
@@ -23,7 +23,7 @@ class LRScheduler(_LRScheduler):
         # Check if using mixed precision training
         self.mixed_training = False
         base_optimizer = optimizer
- 
+
         # Check that optimizer param is valid
         if not isinstance(optimizer, Optimizer):
             raise TypeError('{} is not an Optimizer'.format(
@@ -99,7 +99,7 @@ class LinearWarmUpScheduler(LRScheduler):
         if progress < self.warmup:
             return [base_lr * progress / self.warmup for base_lr in self.base_lrs]
         else:
-            return [base_lr * max(( progress - 1.0)/(self.warmup - 1.0), 0.) for base_lr in self.base_lrs]
+            return [base_lr * max((progress - 1.0)/(self.warmup - 1.0), 0.) for base_lr in self.base_lrs]
 
 
 class PolyWarmUpScheduler(LRScheduler):
@@ -120,7 +120,9 @@ class PolyWarmUpScheduler(LRScheduler):
         else:
             self.last_epoch = 1
 
+        print("self.last_epoch=%d" % (self.last_epoch))
         for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+            print("update lr=%.8f" % lr)
             param_group['lr'] = lr
 
     def get_lr(self):
