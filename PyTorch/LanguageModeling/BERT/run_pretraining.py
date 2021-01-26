845d27096c98fd5c29eab6e63b423a6ce73fdbb1
vycezhong <izhongyuchen@gmail.com>
2021-01-27 00:50:48 +0800

    fix

--- PyTorch/LanguageModeling/BERT/run_pretraining.py
+++ PyTorch/LanguageModeling/BERT/run_pretraining.py
@@ -682,7 +682,7 @@ def main():
                     if not torch.isfinite(loss):
                         print("[step %d]\tlocal rank %d loss is %f!!!" %
                               (global_step, bps.local_rank(), loss.item()))
-                        loss = 0  # skip
+                        loss = torch.tensor(0).cuda()  # skip
 
                     divisor = args.gradient_accumulation_steps
                     if args.gradient_accumulation_steps > 1:
