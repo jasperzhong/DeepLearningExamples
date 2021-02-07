5513bb58fd0cb99d48b918b0fc972995fa85b0df
zhongyuchen <izhongyuchen@gmail.com>
2021-02-07 22:34:12 +0800

    fix script

--- PyTorch/LanguageModeling/BERT/scripts/run_glue.sh
+++ PyTorch/LanguageModeling/BERT/scripts/run_glue.sh
@@ -61,7 +61,7 @@ else
 fi
 
 CMD="python3 $mpi_command run_glue.py "
-CMD+="--task_name ${task_name} "
+CMD+="--task_name ${task_name,,} "
 if [[ $mode == *"train"* ]] ; then
   CMD+="--do_train "
   CMD+="--train_batch_size=$batch_size "
